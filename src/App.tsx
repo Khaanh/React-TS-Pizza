3f4aa060fca66faa1ce95ced8fa731e104d175a2
Khanh <Khanh13Nguyen13@gmail.com>
2024-09-18 19:51:20 +0200

    Added fonts and img

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// .poppins-light {
+//   font-family: "Poppins", sans-serif;
+//   font-weight: 300;
+//   font-style: normal;
+// }
+// Roboto: CSS classes
+
+// .roboto-thin {
+//   font-family: "Roboto", sans-serif;
+//   font-weight: 100;
+//   font-style: normal;
+// }
+
+// .roboto-regular {
+//   font-family: "Roboto", sans-serif;
+//   font-weight: 400;
+//   font-style: normal;
+// }
+
+function App() {
+	return <></>;
+}
+
+export default App;
